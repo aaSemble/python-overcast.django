b3686e4974d95f1850de2cafc1e37f87ca90b0f0
root <root@jas-VirtualBox.(none)>
2015-12-11 13:40:16 +0530

    Refactored - removed url hard coded values

--- aasemble/django/apps/buildsvc/tests_functional.py
+++ aasemble/django/apps/buildsvc/tests_functional.py
@@ -93,7 +93,8 @@ class RepositoryFunctionalTests(StaticLiveServerTestCase):
         ''' This tests validates if non public mirror is created'''
         new_mirror_button = (by.By.LINK_TEXT, 'New')
         self.create_login_session('brandon')
-        self.selenium.get('%s%s' % (self.live_server_url, '/mirrorsvc/mirrors/'))
+        self.selenium.get(self.live_server_url)
+        self.selenium.find_element(by.By.LINK_TEXT, 'Mirrors').click()
         self.selenium.set_window_size(1024, 768)
         self.assertTrue(self._is_element_visible(new_mirror_button), "Mirror New Button is not Visible")
         self.selenium.find_element(*new_mirror_button).click()
